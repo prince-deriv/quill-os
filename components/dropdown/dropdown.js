b2bdec520d05c479b698fcfbbd146aca765b3757
meenakshi-deriv <meenakshi@regentmarkets.com>
2024-05-22 17:22:36 +0400

    dropdown

--- components/dropdown/dropdown.js
+++ components/dropdown/dropdown.js
@@ -21,8 +21,22 @@ $(document).ready(() => {
       text: "wontfix",
     },
   ];
-
   $("#test").select2({
     data: data,
+  });
+  $("#test1").select2({
+    data: data,
+  });
+  
+  // Event listener for when the dropdown is opened
+  $("#test").on('select2:open', function (e) {
+    setTimeout(function() {
+      $('.select2-results__option').css('min-height', '48px');
+    }, 0);
+  });
+  $("#test1").on('select2:open', function (e) {
+    setTimeout(function() {
+      $('.select2-results__option').css('min-height', '32px');
+    }, 0);
   });
 });

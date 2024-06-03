6f4ba83dde754924b36dae4ceaac2d3f4cbe692f
meenakshi-deriv <meenakshi@regentmarkets.com>
2024-06-03 10:14:07 +0400

    added status icon and status messages

--- components/dropdown/dropdown.js
+++ components/dropdown/dropdown.js
@@ -1,4 +1,3 @@
-
 $(document).ready(() => {
   const data = [
     { id: 0, text: "enhancement" },
@@ -7,10 +6,14 @@ $(document).ready(() => {
     { id: 3, text: "invalid", disabled: true },
     { id: 4, text: "wontfix" },
   ];
-let textAlignment = 'center';
+  let textAlignment = 'center';
+
+  // SVG Icons for success and error states
+  const successIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="24" height="24" role="img"><path d="M16 8.375c-2.93 0-5.586 1.563-7.07 4.063-1.446 2.539-1.446 5.624 0 8.124A8.13 8.13 0 0 0 16 24.626c2.89 0 5.547-1.523 7.031-4.062 1.446-2.5 1.446-5.586 0-8.125-1.484-2.5-4.14-4.063-7.031-4.063M16 26.5c-3.594 0-6.875-1.875-8.672-5-1.797-3.086-1.797-6.875 0-10 1.797-3.086 5.078-5 8.672-5 3.555 0 6.836 1.914 8.633 5 1.797 3.125 1.797 6.914 0 10a9.93 9.93 0 0 1-8.633 5m4.414-11.836-5 5c-.39.39-.976.39-1.328 0l-2.5-2.5a.856.856 0 0 1 0-1.289c.351-.39.937-.39 1.328 0l1.836 1.836 4.336-4.336c.352-.39.937-.39 1.328 0a.92.92 0 0 1 0 1.29"></path></svg>`;
+  const errorIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="24" height="24" role="img"><path d="M15.688 9.82 7.953 22.516c-.078.117-.078.195-.078.312 0 .313.234.547.547.547h15.117c.313 0 .586-.234.586-.547 0-.117-.04-.195-.117-.312L16.273 9.82A.29.29 0 0 0 16 9.625c-.156 0-.234.078-.312.195m-1.602-.976A2.25 2.25 0 0 1 16 7.75c.742 0 1.484.43 1.875 1.094l7.734 12.695c.235.39.391.82.391 1.29 0 1.327-1.094 2.421-2.46 2.421H8.421A2.434 2.434 0 0 1 6 22.828q0-.703.352-1.289zm3.164 12.031c0 .703-.586 1.25-1.25 1.25-.703 0-1.25-.547-1.25-1.25 0-.664.547-1.25 1.25-1.25.664 0 1.25.586 1.25 1.25m-.312-7.187v3.75c0 .546-.43.937-.938.937-.547 0-.937-.39-.937-.937v-3.75c0-.508.39-.938.937-.938a.95.95 0 0 1 .938.938"></path></svg>`;
+
   // Initialize Select2 with custom template
   function initializeSelect2(elementId, sizeClass) {
-
     $(`#${elementId}`).select2({
       data: data,
       templateResult: function (data, container) {
@@ -29,11 +32,25 @@ let textAlignment = 'center';
   // Function to update classes for success or error state
   function updateStateClass(elementId, state) {
     const $container = $(`#${elementId}`).next('.select2-container');
+    const $selection = $container.find('.select2-selection');
+    const $iconContainer = $selection.find('.state-icon-container');
+
+    // Remove existing icons
+    $iconContainer.remove();
+
+    // Create a new icon container
+    const $newIconContainer = $('<span class="state-icon-container state-icon"></span>');
+
     if (state === 'success') {
       $container.removeClass('quill-dropdown-error').addClass('quill-dropdown-success');
+      $newIconContainer.html(successIcon);
     } else if (state === 'error') {
       $container.removeClass('quill-dropdown-success').addClass('quill-dropdown-error');
+      $newIconContainer.html(errorIcon);
     }
+
+    // Insert the icon container before the arrow icon
+    $selection.find('.select2-selection__arrow').before($newIconContainer);
   }
 
   // Button event handlers for success and error states
@@ -45,7 +62,6 @@ let textAlignment = 'center';
   // Function to update text alignment
   function updateTextAlignment(elementId, alignment) {
     textAlignment = alignment;
-    console.log('textAlignment',textAlignment);
     $(`#${elementId}`).next('.select2-container')
       .removeClass('text-align-start text-align-center')
       .addClass(`text-align-${alignment}`);
@@ -53,7 +69,6 @@ let textAlignment = 'center';
     $(`#${elementId}`).find('option').each(function () {
       $(this).data('data').element.className = `text-align-${alignment}`;
     });
-   
 
     initializeSelect2(elementId, elementId === 'test' ? 'md' : 'sm');
   }

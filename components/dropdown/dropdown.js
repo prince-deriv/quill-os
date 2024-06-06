d6e9e9df085b59bfe6370e348d1fff9f4d7c33dc
meenakshi-deriv <meenakshi@regentmarkets.com>
2024-06-06 12:42:58 +0400

    fix: fixed alignment issue

--- components/dropdown/dropdown.js
+++ components/dropdown/dropdown.js
@@ -1,14 +1,15 @@
 $(document).ready(() => {
   $("html").addClass("light");
   const data = [
-    { id: 0, text: "enhancement" },
+    { id: 0, text: "enhancement" , selected: true},
     { id: 1, text: "bug" },
     { id: 2, text: "duplicate" },
     { id: 3, text: "invalid", disabled: true },
     { id: 4, text: "wontfix" },
   ];
-  let textAlignment = 'start';
   const hasLabel = $('#has-label').length;
+  const textAlignment = hasLabel ? 'start' : 'center';
+ 
 
   const successIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="24" height="24" role="img"><path d="M16 8.375c-2.93 0-5.586 1.563-7.07 4.063-1.446 2.539-1.446 5.624 0 8.124A8.13 8.13 0 0 0 16 24.626c2.89 0 5.547-1.523 7.031-4.062 1.446-2.5 1.446-5.586 0-8.125-1.484-2.5-4.14-4.063-7.031-4.063M16 26.5c-3.594 0-6.875-1.875-8.672-5-1.797-3.086-1.797-6.875 0-10 1.797-3.086 5.078-5 8.672-5 3.555 0 6.836 1.914 8.633 5 1.797 3.125 1.797 6.914 0 10a9.93 9.93 0 0 1-8.633 5m4.414-11.836-5 5c-.39.39-.976.39-1.328 0l-2.5-2.5a.856.856 0 0 1 0-1.289c.351-.39.937-.39 1.328 0l1.836 1.836 4.336-4.336c.352-.39.937-.39 1.328 0a.92.92 0 0 1 0 1.29"></path></svg>`;
   const errorIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="24" height="24" role="img"><path d="M15.688 9.82 7.953 22.516c-.078.117-.078.195-.078.312 0 .313.234.547.547.547h15.117c.313 0 .586-.234.586-.547 0-.117-.04-.195-.117-.312L16.273 9.82A.29.29 0 0 0 16 9.625c-.156 0-.234.078-.312.195m-1.602-.976A2.25 2.25 0 0 1 16 7.75c.742 0 1.484.43 1.875 1.094l7.734 12.695c.235.39.391.82.391 1.29 0 1.327-1.094 2.421-2.46 2.421H8.421A2.434 2.434 0 0 1 6 22.828q0-.703.352-1.289zm3.164 12.031c0 .703-.586 1.25-1.25 1.25-.703 0-1.25-.547-1.25-1.25 0-.664.547-1.25 1.25-1.25.664 0 1.25.586 1.25 1.25m-.312-7.187v3.75c0 .546-.43.937-.938.937-.547 0-.937-.39-.937-.937v-3.75c0-.508.39-.938.937-.938a.95.95 0 0 1 .938.938"></path></svg>`;
@@ -66,17 +67,19 @@ $(document).ready(() => {
 
   // Function to update text alignment
   function updateTextAlignment(elementId, alignment) {
-    textAlignment = alignment;
+    const hasLabel = $(`#${elementId}`).parent().find('#has-label').length > 0;
+    const textAlignment = hasLabel ? 'start' : alignment;
+
     $(`#${elementId}`).next('.select2-container')
-      .removeClass('text-align-start text-align-center')
-      .addClass(`text-align-${alignment}`);
+        .removeClass('text-align-start text-align-center')
+        .addClass(`text-align-${textAlignment}`);
 
     $(`#${elementId}`).find('option').each(function () {
-      $(this).data('data').element.className = `text-align-${alignment}`;
+        $(this).data('data').element.className = `text-align-${textAlignment}`;
     });
 
     initializeSelect2(elementId, elementId === 'test' ? 'md' : 'sm');
-  }
+}
 
   // Button event handlers for text alignment
   $('#triggerTestAlignLeft').on('click', () => updateTextAlignment('test', 'start'));
@@ -113,12 +116,14 @@ $(document).ready(() => {
     });
   }
   var element = $('.select2-selection__rendered#select2-test-container');
-  if (hasLabel) {
-    console.log('inside',element.text())
+
+  if (element.children().length > 0 && hasLabel) {
+
     $('.quill-dropdown-source').closest('.quill-dropdown-md').removeClass('focused label-focused');
   }
   else {
-    $('.quill-dropdown-source').closest('.quill-dropdown-md').addClass('focused');
+    const addClass = hasLabel ? 'focused label-focused' : 'focused'
+    $('.quill-dropdown-source').closest('.quill-dropdown-md').addClass(addClass);
   }
 
 });

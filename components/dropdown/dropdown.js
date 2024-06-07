0c3f1ada7fdd465d624d9441379e530b272b63ff
meenakshi-deriv <meenakshi@regentmarkets.com>
2024-06-07 14:09:28 +0400

    added placeholder icon

--- components/dropdown/dropdown.js
+++ components/dropdown/dropdown.js
@@ -13,28 +13,41 @@ $(document).ready(() => {
   if (hasLabel) {
     $('.quill-dropdown-source').closest('.quill-dropdown-md').addClass('label');
   }
+
   // Initialize Select2 with custom template
   function initializeSelect2(elementId, sizeClass) {
+    const hasLabelInitial = $(`#${elementId}`).closest('.quill-dropdown-container').find('#has-label').length > 0;
+    console.log('hasLabel param change', hasLabelInitial);
     const hasLabel = $(`#${elementId}`).parent().find('#has-label').length > 0;
     const textAlignment = hasLabel ? 'start' : 'center'; // Default alignment to 'center' if no label
-
+  
     $(`#${elementId}`).select2({
-        data: data,
-        placeholder: "Select",
-        templateResult: function (data, container) {
-            $(container).addClass(`select2-results__option--${sizeClass} dropdown-align-${textAlignment}`).removeClass(`select2-results__option--selected`);
-            if (data.selected) {
-                $(container).addClass('select2-results__option--selected');
-            }
-            return data.text;
+      data: data,
+      placeholder: "Select",
+      templateResult: function (data, container) {
+        $(container).addClass(`select2-results__option--${sizeClass} dropdown-align-${textAlignment}`).removeClass(`select2-results__option--selected`);
+        if (data.selected) {
+          $(container).addClass('select2-results__option--selected');
         }
+        return data.text;
+      }
     });
-
+  
+    const iconHtml = '<span class="quill-icon-container"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="24" height="24" role="img"><path d="M22.094 22.79a8.76 8.76 0 0 0 2.656-6.29c0-4.805-3.945-8.75-8.75-8.75-4.844 0-8.75 3.945-8.75 8.75a8.89 8.89 0 0 0 2.617 6.29c.547-2.15 2.54-3.79 4.883-3.79h2.5c2.305 0 4.297 1.64 4.844 3.79m-1.133.898v.039c-.156-1.954-1.758-3.477-3.711-3.477h-2.5A3.754 3.754 0 0 0 11 23.727c1.406.976 3.125 1.523 5 1.523 1.836 0 3.555-.547 4.96-1.562M16 26.5c-3.594 0-6.875-1.875-8.672-5-1.797-3.086-1.797-6.875 0-10 1.797-3.086 5.078-5 8.672-5 3.555 0 6.836 1.914 8.633 5 1.797 3.125 1.797 6.914 0 10a9.93 9.93 0 0 1-8.633 5m0-10c.664 0 1.25-.352 1.602-.937.351-.547.351-1.29 0-1.876-.352-.546-.938-.937-1.602-.937-.703 0-1.29.39-1.64.938-.352.585-.352 1.328 0 1.874A1.87 1.87 0 0 0 16 16.5m-3.125-1.875c0-1.094.586-2.11 1.563-2.695.937-.547 2.148-.547 3.124 0 .938.586 1.563 1.601 1.563 2.695 0 1.133-.625 2.148-1.562 2.734-.977.547-2.188.547-3.125 0a3.16 3.16 0 0 1-1.563-2.734"></path></svg></span>';
+    const $selection = $(`#${elementId}`).next('.select2-container').find('.select2-selection');
+    $selection.prepend(iconHtml); 
+    const has_icon = $selection.find('.quill-icon-container').length > 0;
+    if (has_icon) {
+      $selection.closest('.quill-dropdown-container').addClass('has-icon').removeClass('has-not-icon');
+    }
+    else {
+       $selection.closest('.quill-dropdown-container').addClass('has-not-icon').removeClass('has-icon');
+    }
     // Update text alignment of the select2 container
     $(`#${elementId}`).next('.select2-container')
-        .removeClass('text-align-start text-align-center')
-        .addClass(`text-align-${textAlignment}`);
-}
+      .removeClass('text-align-start text-align-center')
+      .addClass(`text-align-${textAlignment}`);
+  }
 
   initializeSelect2('test', 'md');
   initializeSelect2('test1', 'sm');
